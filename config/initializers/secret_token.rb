c365f441e8097bc236218194fef4fc16cded415a
Kevin Raphael Pineda <rapaelito@Raphazard.(none)>
2013-05-16 14:42:40 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MdoInventory::Application.config.secret_token = 'dea384902aa658824522c5877a27c760bdfbecee2520eacaff9019390422227d29dcb59891a3499d0d0d49704f67cb2f6a36a8f5d05eda28722e1915c4aa55f3'
