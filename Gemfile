7092f30eac0c8759ffd22d01ac1f75529d467768
Raphael Pineda <dothacker495@yahoo.com>
2013-05-17 14:29:12 +0800

    Added Paperclip photo attachments in the products

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ group :assets do
 
   gem 'uglifier', '>= 1.0.3'
 end
-
+gem 'paperclip', '3.1.0'
 gem 'jquery-rails'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :owner, :quantity
+  attr_accessible :name, :owner, :quantity, :photo
+  has_attached_file :photo,
+                  :default_url => "/assets/missing.gif",
+                  :url  => "/assets/products/:id/:style/:basename.:extension",
+                  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
   validates :name, :owner, :quantity, :presence => true
   validates :quantity, :numericality => {greater_than: 0}
   def self.checkElseUpdate product
