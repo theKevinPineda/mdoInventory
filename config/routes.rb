1644c5ec4df48af5a4d9e7abd61ae0949c0f6374
Raphael Pineda <dothacker495@yahoo.com>
2013-05-17 11:53:46 +0800

    Minor revision for products#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 MdoInventory::Application.routes.draw do
   resources :products
   root :to => "products#index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
